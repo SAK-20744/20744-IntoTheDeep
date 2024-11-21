077f971e2d8211a18a64a96a2083123bf7d87bee
Paul Berendt <paulberendt@aurora-schools.org>
2024-11-20 19:52:20 -0500

    Fixed errors. End of story.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/LimeLightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/LimeLightTest.java
@@ -1,9 +1,5 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -11,26 +7,28 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
-import java.util.List;
-
 @TeleOp
-public abstract class LimeLightTest extends OpMode {
-    public Limelight3A LL = hardwareMap.get(Limelight3A.class, "Limelight");
+public class LimeLightTest extends OpMode {
+    public Limelight3A LL;
 
-    public void runOpMode() throws InterruptedException {
+    @Override
+    public void init() {
+        LL = hardwareMap.get(Limelight3A.class, "Limelight");
         LL.pipelineSwitch(0);
         LL.start();
-        while (linearOpMode.opModeIsActive()) {
-            LLResult result = LL.getLatestResult();
-            if (result != null) {
-                if (result.isValid()) {
-                    Pose3D botpose = result.getBotpose();
-                    List SamplePosition = result.getColorResults();
-                    telemetry.addData("tx", result.getTx());
-                    telemetry.addData("ty", result.getTy());
-                    telemetry.addData("Sample:", SamplePosition.toString());
-                    telemetry.update();
-                }
+    }
+
+    @Override
+    public void loop() {
+        LLResult result = LL.getLatestResult();
+        if (result != null) {
+            if (result.isValid()) {
+                Pose3D YellowSamplePose = result.getBotpose();
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("Sample:", YellowSamplePose.toString());
+                telemetry.addData("Image", result);
+                telemetry.update();
             }
         }
     }
