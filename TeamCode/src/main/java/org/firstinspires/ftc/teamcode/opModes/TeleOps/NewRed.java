e4a5cba3e34f61dc5d887561014dc546874c93f6
SAK26 <akileshkumar@aurora-schools.org>
2025-07-18 17:30:37 -0400

    manual sample claw v1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOps/NewRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOps/NewRed.java
@@ -346,7 +346,7 @@ public class NewRed extends OpMode {
                 } else
                     clawTarget = CLAW_CLOSED;
             } else {
-                if (gamepad2.right_bumper && !gamepad1.left_bumper || readyToTransfer) {
+                if ((gamepad1.left_trigger > 0.5 || gamepad2.right_bumper) && !gamepad1.left_bumper || readyToTransfer) {
                     clawTarget = CLAW_CLOSED;
                 } else {
                     clawTarget = CLAW_OPEN;
